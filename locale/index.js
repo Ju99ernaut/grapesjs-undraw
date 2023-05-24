46e0c2c9f74cbb186cb6a6146decb0f028828515
Brendon Ngirazi <48953676+Ju99ernaut@users.noreply.github.com>
2023-05-24 10:36:58 +0200

    Build :package:

---
+++ locale/index.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var _interopRequireDefault = require("@babel/runtime/helpers/interopRequireDefault");
+
+Object.defineProperty(exports, "__esModule", {
+  value: true
+});
+Object.defineProperty(exports, "en", {
+  enumerable: true,
+  get: function get() {
+    return _en.default;
+  }
+});
+
+var _en = _interopRequireDefault(require("./en"));
